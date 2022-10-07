335350bc49d9707d5a2fa338974e9beaa12dd823
sdidasheva <ddsamals97@gmail.com>
2022-10-07 16:29:34 +0600

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import react from "react";
-import Background from "../app/components/Background/Background";
 import Preloader from "../app/components/Preloader/Preloader";
 
 export default function Home() {
 	return (
 		<>
 			<Preloader />
-			{/* <Background /> */}
 		</>
 	);
 }
